fe57e22a6bf5f11d5712d64f998062a4f3302140
lisawilliams <lisa@placeblogger.com>
2017-04-29 17:57:05 -0400

    added objects and functions to main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
- console.log("Up and running!");
 
+// array for cards
+var cards = [
 
-var cards = ["queen","queen","king","king"];
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
 
-var cardsInPlay =[];
-var cardOne = cards[0];
-var cardTwo = cards[1];
+},
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
 
-if (cardsInPlay.length === 2) {
+];
+
+// array for cards in play
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	// do they match?
 
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -21,3 +41,34 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.")
 	}
 }
+
+
+var flipCard = function (cardID) {
+	console.log("User flipped " + cards[cardID].rank);
+	//console.log(cardImage);
+	//console.log(cards[suit]);
+
+	// add card to array of cardsInPlay
+
+	cardsInPlay.push(cards[cardID].rank);
+
+	// show the what image is up in console
+
+	console.log(cards[cardID].cardImage);
+
+	// display suit in console
+
+	console.log(cards[cardID].suit);
+
+	// see if two cards have been played
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
+};
+
+flipCard(0);
+flipCard(2);
+  
+
